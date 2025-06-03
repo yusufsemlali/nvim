1d04e062f8d9c5d04b452f51cc9b5f994a06ad24
yusufsemlali <ysfm.102@gmail.com>
2025-06-03 08:11:20 +0100

    added prettier formatting support

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,21 @@
+-- ~/.config/nvim/lua/custom/configs/conform.lua
+
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    -- css = { "prettier" },
-    -- html = { "prettier" },
+    typescript = { "prettier" },
+    typescriptreact = { "prettier" },
+    javascript = { "prettier" },
+    javascriptreact = { "prettier" },
+    json = { "prettier" },
+    html = { "prettier" },
+    css = { "prettier" },
   },
 
-  -- format_on_save = {
-  --   -- These options will be passed to conform.format()
-  --   timeout_ms = 500,
-  --   lsp_fallback = true,
-  -- },
+  format_on_save = {
+    timeout_ms = 500,
+    lsp_fallback = true,
+  },
 }
 
 return options

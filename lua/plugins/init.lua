311f6b139e87ba6c8be84e91c29a2a00407b1a12
yusufsemlali <ysfm.102@gmail.com>
2025-06-05 12:23:49 +0100

    configured vim-visual-multi

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -2,13 +2,21 @@ return {
   {
     "stevearc/conform.nvim",
     -- event = 'BufWritePre', -- uncomment for format on save
-    opts = require "configs.conform",
+    opts = require   "configs.conform",
   },
 
   -- These are some examples, uncomment them if you want to see them work!
   {"neovim/nvim-lspconfig",config = function()require "configs.lspconfig"end,},
   {"Pocco81/auto-save.nvim", lazy = false},
-  {"mg979/vim-visual-multi",branch = "master",lazy = false, config = function()end,},
+  {"mg979/vim-visual-multi",branch = "master",lazy = false,
+    config = function()
+      vim.g.VM_maps = {
+        ["Find Under"]         = "<C-d>",
+        ["Find Subword Under"] = "<C-d>",
+        ["Select All"]         = "<Leader>A",
+        ["Add Cursor Down"]    = "<C-Down>",
+        ["Add Cursor Up"]      = "<C-Up>",
+  }end,}
 
   -- {
   -- 	"nvim-treesitter/nvim-treesitter",
